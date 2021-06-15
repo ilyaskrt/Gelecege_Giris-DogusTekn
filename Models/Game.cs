37b10c55b30ad01db5e482aa05530fc94faa7112
İlyas <ilyaskrt19@gmail.com>
2021-06-15 13:19:10 +0300

    Add files via upload

---
+++ Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WhatIsDI.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal ListPrice { get; set; }
+
+    }
+}
